3039c4e3b329811021c11409207d3cc8901e9668
MarifeGarcia <147678876+MarifeGarcia@users.noreply.github.com>
2023-10-18 15:41:07 +0800

    Add files via upload

---
+++ script8.js
@@ -0,0 +1,21 @@
+//Favorite Numbers
+const favSingers = ['Moira', 'Jessie', 'taylor']; 
+//statement1
+ console.log(favSingers); 
+ 
+//Favorite Numbers  
+ const favNumbers = ['21', '24', '04', '27']; 
+ //statement2
+ console.log(favNumbers); 
+  
+ const mixedArr = ["string",["otherarray"], 123, true]; 
+  
+ console.log(favSingers[0]); 
+  
+ console.log(favSingers[1]); 
+  
+ console.log(favSingers[2]); 
+  
+ console.log(favNumbers[1]); 
+  
+ console.log(mixedArr[3]);
